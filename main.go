447063e06655cd0fbaa02d50e8bb9f91ad3d51cb
diogopaza <diogopazacvel@gmail.com>
2019-06-19 14:54:37 -0300

    creating endpoints

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import(
+
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+
+)
+
+type User struct{
+
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+}
+
+type JwtToken struct{
+	Token string `json:"token"`
+}
+
+type Exception struct{
+	Message string `json:message`
+}
+
+func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request){
+
+	w.Write([]byte("create end point"))
+}
+
+func ProtectedEndpoint(w http.ResponseWriter, req *http.Request){
+
+	w.Write([]byte("protected end point"))
+}
+
+
+func main(){
+
+	router := mux.NewRouter()
+	fmt.Println("Starting the application....")
+	
+	router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
+	router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
+	log.Fatal(http.ListenAndServe(":12345", router))
+
+
+
+
+}
